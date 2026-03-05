ba3978df2f787d5b6e5bed6fa477af1fa7c1cc15
Amarachi <umaolamma@gmail.com>
2026-03-05 21:16:29 +0100

    build fix

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -3,7 +3,7 @@ import Header from "@/components/layout/header/Header";
 import About12 from "@/components/sections/About/About12";
 import Cta1 from "@/components/sections/cta/Cta1";
 import HeroInner from "@/components/sections/heros/HeroInner";
-import History1 from "@/components/sections/history/History1";
+// import History1 from "@/components/sections/history/History1";
 import TjMagicCursor from "@/components/shared/others/TjMagicCursor";
 import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
 export default function History() {
@@ -14,7 +14,7 @@ export default function History() {
 			<main>
 				<HeroInner title={"Company history"} text={"Company history"} />
 				<About12 />
-				<History1 />
+				{/* <History1 /> */}
 				<Cta1 />
 			</main>
 			<Footer footerType={"inner"} />
