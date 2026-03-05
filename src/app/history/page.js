f91539a7507d2dba3c1d95f4b10c7a867e463468
Amarachi <umaolamma@gmail.com>
2026-03-05 21:23:22 +0100

    changes

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -1,6 +1,6 @@
 import Footer from "@/components/layout/footer/Footer";
 import Header from "@/components/layout/header/Header";
-import About12 from "@/components/sections/About/About12";
+// import About12 from "@/components/sections/About/About12";
 import Cta1 from "@/components/sections/cta/Cta1";
 import HeroInner from "@/components/sections/heros/HeroInner";
 // import History1 from "@/components/sections/history/History1";
@@ -13,7 +13,7 @@ export default function History() {
 			<Header isStickyHeader={true} />
 			<main>
 				<HeroInner title={"Company history"} text={"Company history"} />
-				<About12 />
+				{/* <About12 /> */}
 				{/* <History1 /> */}
 				<Cta1 />
 			</main>
